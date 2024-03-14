4e791ebb867fae614320d2348788518bb10031a2
Mikołaj Lubiak <lubiak@proton.me>
2024-03-14 13:43:33 +0100

    Delete prints

--- vlkn_swap_chain.cpp
+++ vlkn_swap_chain.cpp
@@ -386,12 +386,10 @@ VkPresentModeKHR VlknSwapChain::chooseSwapPresentMode(
     const std::vector<VkPresentModeKHR> &availablePresentModes) {
   for (const auto &availablePresentMode : availablePresentModes) {
     if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
-      std::cout << "Present mode: Mailbox" << std::endl;
       return availablePresentMode;
     }
   }
 
-  std::cout << "Present mode: V-Sync" << std::endl;
   return VK_PRESENT_MODE_FIFO_KHR;
 }
 
