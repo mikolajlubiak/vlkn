21783cbb6092aeee776b23ad8fa7154990861bac
Mikołaj Lubiak <lubiak@proton.me>
2024-10-15 23:11:57 +0200

    Normalize forward direction vector

--- keyboard_movement_controller.cpp
+++ keyboard_movement_controller.cpp
@@ -18,8 +18,9 @@ void KeyboardMovementController::moveInPlaneXYZ(GLFWwindow *window, float step,
   float yaw = gameObject.transform.rotation.y;
   float pitch = gameObject.transform.rotation.x;
 
-  const glm::vec3 forwardDir{std::sin(yaw) * std::cos(pitch), -std::sin(pitch),
-                             std::cos(yaw) * std::cos(pitch)};
+  const glm::vec3 forwardDir = glm::normalize(
+      glm::vec3(std::sin(yaw) * std::cos(pitch), -std::sin(pitch),
+                std::cos(yaw) * std::cos(pitch)));
   const glm::vec3 rightDir =
       glm::normalize(glm::cross(glm::vec3{0.0f, 1.0f, 0.0f}, forwardDir));
   const glm::vec3 upDir = glm::normalize(glm::cross(rightDir, forwardDir));
